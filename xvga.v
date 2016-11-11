a25ba655d52f7863777ddf2f415eebffbba8b8eb
Amelia Becker <abecker42@users.noreply.github.com>
2016-11-11 16:57:00 -0500

    Initial upload, keep talking infrastructure
    debounce, sync, "labkit", xvga

---
+++ xvga.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:52:43 11/11/2016 
+// Design Name: 
+// Module Name:    xvga 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module xvga(
+    );
+
+
+endmodule
